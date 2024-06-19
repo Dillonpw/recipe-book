42fe3b034fbef01d34a112c656c086a1faea1974
dillonpw <dillonpwalsh10@gmail.com>
2024-06-19 09:10:09 -0400

    added title

--- app/[title]/page.tsx
+++ app/[title]/page.tsx
@@ -16,12 +16,13 @@ async function getRecipe() {
     return (
       <div className='mx-6 p-2'>
         <h1 className='text-4xl text-center'>{recipe.title}</h1>
-        <h2 className='text-2xl'>Ingredients</h2>
+        <h2 className='text-2xl font-bold'>Ingredients</h2>
         <ul className='list-disc'>
           {recipe.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
           ))}
         </ul>
+        <h2 className='text-2xl font-bold'>Steps</h2>
         <ol className='list-decimal'>
           {recipe.steps.map((step, index) => (
             <li key={index}>{step}</li>
