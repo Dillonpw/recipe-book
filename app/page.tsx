30bc102956c6787b18a7b8b26d6f1caeda34e036
dillonpw <dillonpwalsh10@gmail.com>
2024-06-19 14:07:38 -0400

    added aditional recipe, prep for fuzzy search

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
 
-async function getRecipe() {
-  const prisma = new PrismaClient();
-  const recipe = await prisma.recipe.findFirst();
-  return recipe;
+interface Recipe {
+  id: number;
+  title: string;
+  ingredients: string[];
+  steps: string[];
+  tags: string[];
+  image: string | null;
+}
+
+const prisma = new PrismaClient();
+
+async function getRecipes(): Promise<Recipe[]> {
+  const recipes = await prisma.recipe.findMany();
+  return recipes;
 }
 
 export default async function RecipePage() {
-  const recipe = await getRecipe();
+  const recipes = await getRecipes();
 
-  if (!recipe) {
-    return <div>No recipe found</div>;
+  if (!recipes || recipes.length === 0) {
+    return <div>No recipes found</div>;
   }
 
   return (
     <div className="m-4 h-full text-3xl">
-        <h1 className="text-4xl text-center font-bold">Recipe List</h1>
-      <Link className="un" href={`/${recipe.title}`}>{recipe.title}</Link>
+      <h1 className="text-4xl text-center font-bold">Recipe List</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link className="un" href={`/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
