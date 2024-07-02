4dbec72a5fd321a16cbc6e9939693293c03b340a
dillonpw <dillonpwalsh10@gmail.com>
2024-07-02 10:41:34 -0400

    removing magic link

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import Resend from "next-auth/providers/resend";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
@@ -8,5 +7,5 @@ const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub, Resend],
+  providers: [GitHub],
 });
