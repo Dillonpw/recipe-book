9b9019080e2520ec42f245f3e662d1b2f9fb3205
dillonpw <dillonpwalsh10@gmail.com>
2024-07-11 10:50:24 -0400

    added image, hacky but working

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,13 @@ const Content = () => {
   return (
     <>
       <LoggedIn />
-      <div className="flex h-full items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-center text-2xl font-bold md:text-4xl">
+      <div className="relative flex h-full items-center justify-center">
+        <div className="absolute inset-0 z-0 bg-[url('/bg-img.webp')] bg-[length:700px] bg-bottom bg-no-repeat before:absolute before:inset-0 before:bg-black/10 before:backdrop-blur-sm" />
+        <div className="relative z-10 flex flex-col items-center justify-center gap-4">
+          <h1 className="text-center text-2xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-4xl">
             Welcome To RecipeBook
           </h1>
-          <p className="mx-4 flex-wrap text-center text-xl font-bold md:text-3xl">
+          <p className="mx-4 flex-wrap text-center text-xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-3xl">
             Store and find recipes from all over the world.
           </p>
           <div className="flex flex-col items-center justify-center gap-2 md:flex-row">
@@ -32,4 +33,5 @@ const Content = () => {
     </>
   );
 };
+
 export default Content;
