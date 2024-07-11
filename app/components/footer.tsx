fab6c47e38e79c1ac00dea8cec96ef71968e85f3
dillonpw <dillonpwalsh10@gmail.com>
2024-07-11 10:50:42 -0400

    updated margins added home redirect

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,17 +2,22 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="mt-auto flex h-auto flex-col p-4 shadow-inner">
+    <footer className="flex h-auto flex-col p-4 mt-14">
       <div className="mb-4 flex w-fit flex-col">
-        <p className="font-bold underline mb-2 text-lg">Pages</p>
+        <p className="mb-2 text-lg font-bold underline">Pages</p>
         <ul className="flex flex-col gap-1">
+        <li className="un cursor-pointer">
+            <Link href="/">Home</Link>
+          </li>
           <li className="un cursor-pointer">
             <Link href="/new-recipe">Add Recipes</Link>
           </li>
           <li className="un cursor-pointer">
             <Link href="/recipe-list">Recipe List</Link>
           </li>
-          <li><Link href="/social">Social</Link></li>
+          <li className="un cursor-pointer">
+            <Link href="/social">Social</Link>
+          </li>
         </ul>
       </div>
       <div className="mb-4 flex justify-center">
@@ -23,7 +28,7 @@ const Footer = () => {
         >
           Dillonpw
         </Link>
-        <p className="text-center ml-1">Copyright © 2024</p>
+        <p className="ml-1 text-center">Copyright © 2024</p>
       </div>
     </footer>
   );
