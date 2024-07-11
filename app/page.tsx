9d8bf5daf1ca8d756dd71460ab6b74bb95a7b3bd
dillonpw <dillonpwalsh10@gmail.com>
2024-07-11 13:04:31 -0400

    added custom font

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ const Content = () => {
       <div className="relative flex h-full items-center justify-center">
         <div className="absolute inset-0 z-0 bg-[url('/bg-img.webp')] bg-[length:700px] bg-bottom bg-no-repeat before:absolute before:inset-0 before:bg-gray-50/10 before:backdrop-blur-sm" />
         <div className="relative z-10 flex flex-col items-center justify-center gap-4">
-          <h1 className="text-center text-2xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-5xl">
+          <h1 className="title text-center text-2xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-5xl">
             Welcome To RecipeBook
           </h1>
-          <p className="mx-4 flex-wrap text-center text-xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-3xl">
+          <p className="title mx-4 flex-wrap text-center text-xl font-bold text-gray-50 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,4)] md:text-3xl">
             Store and find recipes from all over the world.
           </p>
           <div className="flex flex-col items-center justify-center gap-2 md:flex-row">
